bfe3811b13753eb7cd3aa55b920ad03baddc4571
Aliaksandr Kalymaha <kolymago.a@gmail.com>
2016-10-18 16:22:40 +0300

    Add application context

---
+++ kurs/App_Context/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using Autobase.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Autobase.App_Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext() : base("Autobase")
+        { }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Trip> Trips { get; set; }
+    }
+}
